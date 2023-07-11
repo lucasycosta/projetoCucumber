c37838b5eac7b38bc12682ecf0036e1b40d3df90
Raoni Karan <raonikg@gmail.com>
2023-07-11 12:04:36 -0300

    Teste

--- src/main/java/com/lucas/usuariocucumber/controller/UsuarioController.java
+++ src/main/java/com/lucas/usuariocucumber/controller/UsuarioController.java
@@ -20,29 +20,29 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("usuario")
-public class UsuarioController{
-	
+public class UsuarioController {
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Usuario cadastrar(@RequestBody Usuario usuario) {
 		log.info("CONTROLLER USUARIO - CADASTRO");
 		return usuarioService.cadastrar(usuario);
 	}
-	
+
 	@GetMapping(value = "/todos", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Usuario> buscarTodos(){
+	public List<Usuario> buscarTodos() {
 		log.info("CONTROLLER USUARIO - BUSCAR TODOS");
 		return usuarioService.buscarTodos();
 	}
-	
+
 	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Usuario> buscar(@RequestParam(value = "id") Long idUsuario) {
 		log.info("CONTROLLER USUARIO - BUSCAR POR ID");
 		return usuarioService.buscar(idUsuario);
 	}
-	
+
 	@DeleteMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public void excluir(@RequestParam(value = "id") Long idUsuario) {
 		log.info("CONTROLLER USUARIO - EXCLUIR");
